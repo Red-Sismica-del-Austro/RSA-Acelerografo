bf2b61b88e4f4d8eab5617d6f83366eeffdb62e4
RSA-Milton <milton.munozc@ucuenca.edu.ec>
2024-11-15 21:33:05 +0000

    FEAT: Implementado nuevo task-script uploadpendingfiles.sh que ejecuta el operation-script subir_archivos_pendientes.py. Actualizado el crontab.

---
+++ scripts/task/uploadpendingfiles.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Cargar las variables de entorno
+source /usr/local/bin/project_paths
+
+# Ejecutar el script
+/usr/bin/python3 $PROJECT_LOCAL_ROOT/scripts/drive/subir_archivos_pendientes.py
+
+exit 0 
