5af9e96f361d9b2ab1d05986a1d2a9f971f61f29
RSA-Milton <milton.munozc@ucuenca.edu.ec>
2025-02-17 21:56:07 +0000

    WIP: Se actualizo el instalador de librerias para incluir NTP y ObsPy

--- scripts/setup/instalar_librerias.sh
+++ scripts/setup/instalar_librerias.sh
@@ -25,7 +25,44 @@ sudo pip3 install --upgrade oauth2client
 # Instalacion de Supervisor
 sudo apt-get install supervisor
 
-# Instalacion libreria WirinPi
-#cd /tmp
-#wget https://project-downloads.drogon.net/wiringpi-latest.deb
-#sudo dpkg -i wiringpi-latest.deb
+# Instalacion de NTP
+sudo apt install ntp -y
+sudo apt install ntpstat -y
+
+# Instalacion de Obspy
+while true; do
+    read -p "Desea continuar con la instalación de ObsPy? (s/n) " response
+    case "$response" in
+        s|S)
+            echo "Instalando dependencias necesarias..."
+            sudo apt-get install -y \
+                python3-pip python3-dev \
+                python3-scipy python3-lxml python3-setuptools \
+                python3-sqlalchemy python3-decorator python3-requests \
+                python3-packaging python3-pyproj python3-pytest \
+                python3-geographiclib python3-cartopy python3-pyshp \
+                libatlas-base-dev libopenblas-dev gfortran
+
+            echo "Actualizando pip..."
+            sudo pip3 install --upgrade pip
+
+            echo "Actualizando NumPy y Matplotlib..."
+            sudo pip3 install --upgrade numpy matplotlib
+
+            echo "Instalando ObsPy..."
+            sudo pip3 install obspy
+
+            echo "Comprobando instalación de ObsPy..."
+            python3 -c "import obspy; print(obspy.__version__)"
+
+            break  # Salir del bucle después de la instalación
+            ;;
+        n|N)
+            echo "Instalación de ObsPy cancelada."
+            break  # Salir del bucle sin instalar
+            ;;
+        *)
+            echo "Opción no válida, por favor ingrese 's' para sí o 'n' para no."
+            ;;
+    esac
+done
