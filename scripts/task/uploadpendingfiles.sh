c5123f3f06d25ae1395874686205d0d02c51f2fc
RSA-Milton <milton.munozc@ucuenca.edu.ec>
2025-11-25 21:33:11 +0000

    REFACTOR: Eliminar scripts que no se utilizan.
    Se elimino el script scripts/task/uploadpendingfiles.sh

--- scripts/task/uploadpendingfiles.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Cargar las variables de entorno
-source /usr/local/bin/project_paths
-
-# Ejecutar el script
-/usr/bin/python3 $PROJECT_LOCAL_ROOT/scripts/drive/subir_pendientes_drive.py
-
-exit 0 
